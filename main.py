f2c4e2c2c1b91993ef118d15e42a531145475276
Alex Todd <alex.todd40@gmail.com>
2019-11-30 15:34:40 +0000

    rewritten hrsg input by defining set of HeatExchangers

--- main.py
+++ main.py
@@ -22,7 +22,10 @@ gt_isentropic_compressor_efficiency = .85
 gt_pressure_ratio = 20.1
 
 steamCycle = steam.SteamCycle(hp, ip, lp, m1, m2, m3, ma, 565, 15)
+steamCycle.SaveResults('steam_data', 'work_data')
+
 gasTurbine = gt.GasTurbine(14.2, 50, 50000, gt_pressure_ratio, gt_isentropic_turbine_efficiency, gt_isentropic_compressor_efficiency)
+gasTurbine.SaveResults("gas_turbine")
 
 fluegas_temp_in = gasTurbine.T[4] + 273.15  # K
 fluegas_massflow = 724
@@ -36,10 +39,11 @@ fluegas_temp_out = fluegas_temp_in - (steamCycle.q_in / (fluegas_massflow * flue
 
 print("HRSGS fluegas outlet temperature: "+str(fluegas_temp_out-273.15)+" C")
 
+
+
 hrsg = HRSG.HRSG(inlet_temp=fluegas_temp_in)
 
-
-# create superheater and add it to the hrsg
+# create exchangers and add them to the hrsg
 
 exchangers = []
 hpsuperheater = HRSG.HeatExchanger(steamCycle.T[13], steamCycle.T[14], fluegas_massflow, steamCycle.superheater_3_mass_flow, 'superheater', steamCycle.P[13])
@@ -54,31 +58,32 @@ lpsuperheater = HRSG.HeatExchanger(steamCycle.T[4], steamCycle.T[5], fluegas_mas
 lpsuperheater.set_name("LP Superheater")
 hrsg.AddExchanger(lpsuperheater)
 
+hpevaporator = HRSG.HeatExchanger(steamCycle.T[12], steamCycle.T[13], fluegas_massflow, steamCycle.evaporator_3_mass_flow, 'evaporator', operating_pressure=steamCycle.P[12], quality_in=0, quality_out=1)
+hpevaporator.set_name("HP Evaporator")
+hrsg.AddExchanger(hpevaporator)
+
+hpeconomiser = HRSG.HeatExchanger(steamCycle.T[11], steamCycle.T[12], fluegas_massflow, steamCycle.economiser_3_mass_flow, 'economiser', steamCycle.P[11])
+hpeconomiser.set_name("HP Economiser")
+hrsg.AddExchanger(hpeconomiser)
+
+ipevaporator = HRSG.HeatExchanger(steamCycle.T[8], steamCycle.T[9], fluegas_massflow, steamCycle.evaporator_2_mass_flow, 'evaporator', operating_pressure=steamCycle.P[8], quality_in=0, quality_out=1)
+ipevaporator.set_name("IP Evaporator")
+hrsg.AddExchanger(ipevaporator)
+
+ipeconomiser = HRSG.HeatExchanger(steamCycle.T[7], steamCycle.T[8], fluegas_massflow, steamCycle.economiser_2_mass_flow, 'economiser', steamCycle.P[7])
+ipeconomiser.set_name("IP Economiser")
+hrsg.AddExchanger(ipeconomiser)
+
+lpevaporator = HRSG.HeatExchanger(steamCycle.T[3], steamCycle.T[4], fluegas_massflow, steamCycle.evaporator_1_mass_flow, 'evaporator', operating_pressure=steamCycle.P[3], quality_in=0, quality_out=1)
+lpevaporator.set_name("LP Evaporator")
+hrsg.AddExchanger(lpevaporator)
+
+lpeconomiser = HRSG.HeatExchanger(steamCycle.T[2], steamCycle.T[3], fluegas_massflow, steamCycle.economiser_1_mass_flow, 'economiser', steamCycle.P[2])
+lpeconomiser.set_name("LP Economiser")
+hrsg.AddExchanger(lpeconomiser)
+
 hrsg.Calculate()
-
-
-'''
-# HP Evaporator
-HRSG['HP_evaporator'] = HeatExchanger(HRSG['LP_superheater'].t['hot out'], steamCycle.T[12], steamCycle.T[13], fluegas_massflow, steamCycle.evaporator_3_mass_flow, 'm', operating_pressure=steamCycle.P[12], quality_in=0, quality_out=1)
-
-# Second IP Superheater
-#HRSG['IP_superheater_2'] = HeatExchanger(HRSG['HP_evaporator'].t['hot out'], steamCycle.T[9], steamCycle.T[15], fluegas_massflow, steamCycle.evaporator_2_mass_flow, 'g', steamCycle.P[9])
-
-# HP Economiser
-HRSG['HP_economiser'] = HeatExchanger(HRSG['HP_evaporator'].t['hot out'], steamCycle.T[11], steamCycle.T[12], fluegas_massflow, steamCycle.economiser_3_mass_flow, 'l', steamCycle.P[11])
-
-# IP Evaporator
-HRSG['IP_evaporator'] = HeatExchanger(HRSG['HP_economiser'].t['hot out'], steamCycle.T[8], steamCycle.T[9], fluegas_massflow, steamCycle.evaporator_2_mass_flow, 'm', operating_pressure=steamCycle.P[8], quality_in=0, quality_out=1)
-
-# IP Economiser
-HRSG['IP_economiser'] = HeatExchanger(HRSG['IP_evaporator'].t['hot out'], steamCycle.T[7], steamCycle.T[8], fluegas_massflow, steamCycle.economiser_2_mass_flow, 'l', steamCycle.P[7])
-
-# LP Evaporator
-HRSG['LP_evaporator'] = HeatExchanger(HRSG['IP_economiser'].t['hot out'], steamCycle.T[3], steamCycle.T[4], fluegas_massflow, steamCycle.evaporator_1_mass_flow, 'm', operating_pressure=steamCycle.P[3], quality_in=0, quality_out=1)
-
-# LP Economiser
-HRSG['LP_economiser'] = HeatExchanger(HRSG['LP_evaporator'].t['hot out'], steamCycle.T[2], steamCycle.T[3], fluegas_massflow, steamCycle.economiser_1_mass_flow, 'l', steamCycle.P[2])
-'''
+hrsg.SaveResults("hrsg")
 
 overallEfficiency = (steamCycle.efficiency + gasTurbine.efficiency) - (steamCycle.efficiency * gasTurbine.efficiency)
 
@@ -87,11 +92,6 @@ print("\n\tSteam Cycle Efficieny: "+str(np.round(100 * steamCycle.efficiency, 1)
 print("\n\tGas Turbine Efficieny: "+str(np.round(100 * gasTurbine.efficiency, 1))+"%")
 print("\n\tTotal plant efficiency: " + str(np.round(100 * overallEfficiency, 1))+"%")
 print("\n\tPlant Net Work: " + str(np.round(gasTurbine.work['Net Work'] + steamCycle.w_net)) + "kW")
-
-
-hrsg.SaveResults("hrsg")
-gasTurbine.SaveResults("gas_turbine")
-steamCycle.SaveResults('steam_data', 'work_data')
 
 # create new figure and axes object
 gas_ts, gas_ax = plt.subplots(1, 1)
